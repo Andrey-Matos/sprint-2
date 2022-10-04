a5c051ad4ebe795260a7d96211e987ad25eb9f1e
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-10-04 17:24:33 -0300

    feat: add splash screen

---
+++ lib/screens/splash.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'homeScreen.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 6);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomeScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/images/splash_logo.png", fit: BoxFit.cover),
+            Image.asset("assets/images/splash_text.png", fit: BoxFit.cover),
+          ],
+        ),
+      ),
+    );
+  }
+}
