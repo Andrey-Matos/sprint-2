bb13b5501c30a8d5dc608fbd060b2d91e2d82f7c
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-10-06 17:01:56 -0300

    Update product_model.dart

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -4,10 +4,16 @@ class Product {
   String title;
   Image image;
   String price;
+  String description;
+  bool isFavorite;
+  List ingredients;
 
   Product({
     required this.title,
     required this.image,
     required this.price,
+    required this.description,
+    required this.isFavorite,
+    required this.ingredients,
   });
 }
