9ce5edc420431516a2ebed0867ac6da82560e4e0
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-10-09 03:45:49 -0300

    mock repo

---
+++ lib/data/productMap.dart
@@ -0,0 +1,11 @@
+import 'package:desafio_2/models/product_model.dart';
+
+Map products = {
+  'comida1': Product(
+      title: 'comida1',
+      description: 'lorem ipsum',
+      isFavorite: false,
+      image: 'honey_lime.png',
+      price: '1000',
+      ingredients: ['esse', 'aquele'])
+};
