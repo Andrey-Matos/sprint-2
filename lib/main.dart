647bd3178d8d52024e97d1ee8ec5bbc551b39412
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-10-11 14:16:08 -0300

    final commit?

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:desafio_2/providers/cartProvider.dart';
 import 'package:desafio_2/screens/addToBasketScreen.dart';
-import 'package:desafio_2/screens/authScreen.dart';
 import 'package:desafio_2/screens/mainScreen.dart';
 import 'package:desafio_2/screens/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,35 +12,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'TT norms pro',
-        primaryColor: Color.fromARGB(255, 255, 164, 81),
-        //backgroundColor: Color.fromARGB(255, 247, 245, 245),
-        textTheme: TextTheme(
-          bodyText2:
-              TextStyle(fontSize: 14, color: Color.fromARGB(255, 39, 33, 77)),
-          button: TextStyle(
-            fontSize: 16,
-            //backgroundColor: Color.fromARGB(255, 255, 164, 81),
-          ),
-        ),
-      ),
-      home: AddToBasketScreen(
-        title: 'comida',
-        price: '1000',
-        ingredients: [
-          'red quinoa',
-          'lime',
-          'honey',
-          'blueberries',
-          'mango',
-          'strawberries',
-          'fresh mint'
-        ],
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
-      ),
-    );
+    return ChangeNotifierProvider(
+        create: (context) => CartProvider(),
+        child: MaterialApp(
+            theme: ThemeData(
+              fontFamily: 'TT norms pro',
+              primaryColor: Color.fromARGB(255, 255, 164, 81),
+              //backgroundColor: Color.fromARGB(255, 247, 245, 245),
+              textTheme: TextTheme(
+                bodyText2: TextStyle(
+                    fontSize: 14, color: Color.fromARGB(255, 39, 33, 77)),
+                button: TextStyle(
+                  fontSize: 16,
+                  //backgroundColor: Color.fromARGB(255, 255, 164, 81),
+                ),
+              ),
+            ),
+            home: Splash()));
   }
 }
